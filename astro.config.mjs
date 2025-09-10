81f320f785e7b65f19c2e28f626f324348673ce8
Anne <annedranowski@gmail.com>
2025-09-10 10:05:01 -0700

    added google analytics to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,40 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 
-// https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
   },
-  site: 'https://example.com',
+  site: 'https://annedranow.xyz', // Update this
+  head: [
+    {
+      tag: 'script',
+      attrs: { 
+        async: true, 
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-C0CBPR3W6J',
+        type: 'text/partytown'
+      }
+    },
+    {
+      tag: 'script',
+      attrs: { type: 'text/partytown' },
+      content: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-C0CBPR3W6J');
+      `
+    }
+  ]
 });
